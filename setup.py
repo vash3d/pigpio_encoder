de18d3e2ea8afed59a926992ae58ea351447e0ad
vash3d <vash.3d@gmail.com>
2019-01-31 01:14:56 +0100

    Changed release state to beta

--- setup.py
+++ setup.py
@@ -9,18 +9,18 @@ with open(path.join(file_path, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='pigpio_encoder',
-      version='0.1.0',
+      version='0.2.0',
       description='Python module for for quickly interface a KY040 rotary encoder with Raspberry Pi.',
       long_description=long_description,
       long_description_content_type='text/markdown',
       classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: GNU GPLv3 Licence',
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3',
-        'Topic :: GPIO interface :: iot',
+        'Topic :: Software Development :: Libraries',
       ],
       keywords='rotary encoder switch ky040 gpio raspberry',
-      url='',
+      url='https://github.com/vash3d/pigpio_encoder',
       author='vash',
       author_email='vash.3d@gmail.com',
       license='GNU GPLv3',
