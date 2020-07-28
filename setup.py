ab36bbbb7ca92c88cc92f99d449b3aa9f40258b2
volker <volker.jaenisch@inqbus.de>
2020-07-28 20:12:29 +0200

    Optimized package structure for easy import.
    from pigpio_encoder install Rotary

--- setup.py
+++ setup.py
@@ -25,6 +25,8 @@ setup(name='pigpio_encoder',
       author='vash',
       author_email='vash.3d@gmail.com',
       license='GNU GPLv3',
-      packages=find_packages(),
+      package_dir={'': 'src'},
+      packages=find_packages(where='src'),
       include_package_data=True,
-      zip_safe=False)
+      zip_safe=False,
+      )
