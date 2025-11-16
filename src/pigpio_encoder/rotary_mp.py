67fa86e723988eb6ee093778102c6c35cca6bfcc
Volker Jaenisch (PhD) <volker.jaenisch@inqbus.de>
2025-11-16 12:25:03 +0100

    new state machine

--- src/pigpio_encoder/rotary_mp.py
+++ src/pigpio_encoder/rotary_mp.py
@@ -1,181 +1,134 @@
-# Rotary encoder class for ESP32 with micropython
-# version: 0.2.5
-
-from machine import Pin, Timer
-import micropython
-
+from machine import Pin
 import time
 
-# States
-dt_gpio1 = 'D'  # dt_gpio is high
-dt_gpio0 = 'd'  # dt_gpio is low
-clk_gpio1 = 'C'  # clk_gpio is high
-clk_gpio0 = 'c'  # clk_gpio is low
-
-# State sequences
-SEQUENCE_UP = dt_gpio1 + clk_gpio1 + dt_gpio0 + clk_gpio0
-SEQUENCE_DOWN = clk_gpio1 + dt_gpio1 + clk_gpio0 + dt_gpio0
-
-timer_init = lambda t, p, cb: t.init(period=p, callback=cb)
-
-
-
+# Rotary encoder FSM transition table (index = (prev_state << 2) + curr_state)
+# Values: +1 (CW step), -1 (CCW step), 0 (no step)
+_STATE_TRANSITION_TABLE = [
+    0,  -1,  1,  0,
+    1,   0,  0, -1,
+   -1,   0,  0,  1,
+    0,   1, -1,  0
+]
 
 class Rotary:
-
-    sequence = ''
-
-    # Default values for the rotary encoder
-    min = 0
-    max = 100
-    scale = 1
-    debounce = 300
-    _counter = 0
-    last_counter = 0
-    rotary_callback = None
-
-    # Default values for the sw_gpioitch
-    sw_gpio_debounce = 300
-    long_press_opt = False
-    sw_gpio_short_callback = None
-    sw_gpio_long_callback = None
-    up_callback = None
-    down_callback = None
-    sw_short_callback = None
-    sw_long_callback = None
-    sw_debounce = None
-
-    wait_time = time.time()
-    long = False
-
-    def __init__(self, clk_gpio=None, dt_gpio=None, sw_gpio=None):
-        if not (clk_gpio and dt_gpio):
-            raise BaseException("clk_gpio and dt_gpio pin must be specified!")
+    def __init__(self, clk_gpio, dt_gpio, sw_gpio=None):
+        if clk_gpio is None or dt_gpio is None:
+            raise ValueError("clk_gpio and dt_gpio must be specified")
 
         self.clk_gpio = clk_gpio
         self.dt_gpio = dt_gpio
-        if sw_gpio is not None:
-            self.sw_gpio = sw_gpio
-        self.setup_pigpio_callbacks()
+        self.sw_gpio = sw_gpio
 
-    def setup_pigpio_callbacks(self):
-        clk_Pin = Pin(self.clk_gpio, Pin.IN, Pin.PULL_UP)
-        self.clk_state = clk_Pin.value()
-        dt_Pin = Pin(self.dt_gpio, Pin.IN, Pin.PULL_UP)
-        self.dt_state = dt_Pin.value()
-        sw_Pin = Pin(self.sw_gpio, Pin.IN, Pin.PULL_UP)
-        self.sw_state = sw_Pin.value()
-        
-        clk_Pin.irq(handler=self.clk_gpio_call, trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING)
-        dt_Pin.irq(handler=self.dt_gpio_call, trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING)
-        
+        self.min = 0
+        self.max = 100
+        self.scale = 1
+        self.debounce = 300  # debounce not used in this implementation but preserved
+
+        self._counter = self.min
+        self.last_counter = self.min
+
+        self.rotary_callback = None
+        self.up_callback = None
+        self.down_callback = None
+
+        self.sw_short_callback = None
+        self.sw_long_callback = None
+        self.long_press_opt = False
+        self.long = False
+        self.sw_debounce = None
+
+        # Initialize pins
+        self.clk_pin = Pin(self.clk_gpio, Pin.IN, Pin.PULL_UP)
+        self.dt_pin = Pin(self.dt_gpio, Pin.IN, Pin.PULL_UP)
         if self.sw_gpio is not None:
-            sw_Pin.irq(handler=self.sw_gpio_call, trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING)
+            self.sw_pin = Pin(self.sw_gpio, Pin.IN, Pin.PULL_UP)
+        else:
+            self.sw_pin = None
 
+        # Initialize the FSM state: 2 bits = (clk << 1) | dt
+        self.prev_state = (self.clk_pin.value() << 1) | self.dt_pin.value()
+
+        # Setup interrupts
+        self.clk_pin.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self._pin_changed)
+        self.dt_pin.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self._pin_changed)
+
+        if self.sw_pin is not None:
+            self.sw_pin.irq(trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING, handler=self._sw_gpio_call)
+        
     @property
     def counter(self):
         return self._counter
-
+    
     @counter.setter
     def counter(self, value):
-        if self._counter != value:
-            self._counter = value
+        clamped_value = max(self.min, min(self.max, value))
+        if clamped_value != self._counter:
+            self._counter = clamped_value
             if self.rotary_callback:
                 self.rotary_callback(self._counter)
-
-    def clk_gpio_call(self, pin):
-        val = pin.value()
-        if val < self.clk_state:
-            self.clk_gpio_fall()
-        elif val > self.clk_state:
-            self.clk_gpio_rise()
-        self.clk_state = val
-
-    def clk_gpio_fall(self ):
-#        print('clk_gpio_fall')
-        if len(self.sequence) > 2:
-            self.sequence = ''
-        self.sequence += clk_gpio1
-
-    def clk_gpio_rise(self): 
-#        print('clk_gpio_rise')
-        self.sequence += clk_gpio0
-        if self.sequence == SEQUENCE_UP:
-            if self.counter < self.max:
+    
+    def _pin_changed(self, pin):
+        current_state = (self.clk_pin.value() << 1) | self.dt_pin.value()
+        index = (self.prev_state << 2) | current_state
+        delta = _STATE_TRANSITION_TABLE[index]
+        self.prev_state = current_state
+        
+        if delta == 1:
+            if self.counter + self.scale <= self.max:
                 self.counter += self.scale
-            if self.up_callback:
-                self.up_callback(self._counter)
-            self.sequence = ''
-
-    def dt_gpio_call(self, pin):
-        val = pin.value()
-        if val < self.dt_state:
-            self.dt_gpio_fall()
-        elif val > self.dt_state:
-            self.dt_gpio_rise()
-        self.dt_state = val
-
-    def dt_gpio_fall(self):
-#        print('dt_gpio_fall')
-        if len(self.sequence) > 2:
-            self.sequence = ''
-        self.sequence += dt_gpio1
-
-    def dt_gpio_rise(self):
-#        print('dt_gpio_rise')
-        self.sequence += dt_gpio0
-        if self.sequence == SEQUENCE_DOWN:
-            if self.counter > self.min:
+                if self.up_callback:
+                    self.up_callback(self._counter)
+        elif delta == -1:
+            if self.counter - self.scale >= self.min:
                 self.counter -= self.scale
-            if self.down_callback:
-                self.down_callback(self._counter)
-            self.sequence = ''
-
-    def sw_gpio_call(self, pin):
+                if self.down_callback:
+                    self.down_callback(self._counter)
+    
+    def _sw_gpio_call(self, pin):
         val = pin.value()
-        if val < self.sw_state:
-            self.sw_gpio_fall()
-        elif val > self.sw_state:
-            self.sw_gpio_rise()
-        self.sw_state = val
-
-    def sw_gpio_rise(self):
-#        print('sw_gpio_rise')
+        if val == 0:  # button pressed
+            self._sw_gpio_fall()
+        else:  # button released
+            self._sw_gpio_rise()
+    
+    def _sw_gpio_rise(self):
         if self.long_press_opt:
             if not self.long:
                 self.short_press()
-
-    def sw_gpio_fall(self):
-#        print('sw_gpio_fall')
+    
+    def _sw_gpio_fall(self):
         if self.long_press_opt:
             self.long = False
-            press_time = time.time()
-            while self.pi.read(self.sw_gpio) == 0:
-                self.wait_time = time.time()
-                time.sleep(0.1)
-                if self.wait_time - press_time > 1.5:
+            start_time = time.ticks_ms()
+            while self.sw_pin.value() == 0:
+                time.sleep(0.01)
+                elapsed = time.ticks_diff(time.ticks_ms(), start_time)
+                if elapsed > 1500:  # long press threshold 1.5 seconds
                     self.long_press()
                     self.long = True
                     break
+            # If comes here and long == False, treat as short press on release
+            if not self.long:
+                self.short_press()
         else:
             self.short_press()
 
-    def setup_rotary(
-            self,
-            rotary_callback=None,
-            up_callback=None,
-            down_callback=None,
-            min=None,
-            max=None,
-            scale=None,
-            debounce=None,
-         ):
-        if not (rotary_callback or up_callback or down_callback):
-            print('At least one callback should be given')
-        # rotary callback has to be set first since the self.counter property depends on it
-        self.rotary_callback = rotary_callback
-        self.up_callback = up_callback
-        self.down_callback = down_callback
+    def short_press(self):
+        if self.sw_short_callback:
+            self.sw_short_callback()
+
+    def long_press(self):
+        if self.sw_long_callback:
+            self.sw_long_callback()
+    
+    def setup_rotary(self, rotary_callback=None, up_callback=None, down_callback=None,
+                     min=None, max=None, scale=None, debounce=None):
+        if rotary_callback:
+            self.rotary_callback = rotary_callback
+        if up_callback:
+            self.up_callback = up_callback
+        if down_callback:
+            self.down_callback = down_callback
         if min is not None:
             self.min = min
             self.counter = self.min
@@ -184,34 +137,22 @@ class Rotary:
             self.max = max
         if scale is not None:
             self.scale = scale
-
-    def setup_switch(self,
-                     sw_short_callback=None,
-                     sw_long_callback=None,
-                     debounce=None,
-                     long_press=None
-                     ):
-        assert sw_short_callback is not None or sw_long_callback is not None
+        if debounce is not None:
+            self.debounce = debounce
+    
+    def setup_switch(self, sw_short_callback=None, sw_long_callback=None,
+                     debounce=None, long_press=None):
         if sw_short_callback is not None:
             self.sw_short_callback = sw_short_callback
         if sw_long_callback is not None:
             self.sw_long_callback = sw_long_callback
         if debounce is not None:
-            self.sw_debounce = debounce
-            self.pi.set_glitch_filter(self.sw_gpio, self.sw_debounce)
+            self.sw_debounce = debounce  # Not used here, placeholder for future debounce logic
         if long_press is not None:
             self.long_press_opt = long_press
-
+    
     @staticmethod
     def watch():
-        """
-        A simple convenience function to have a waiting loop
-        """
         while True:
             time.sleep(10)
 
-    def short_press(self):
-        self.sw_short_callback()
-
-    def long_press(self):
-        self.sw_long_callback()
